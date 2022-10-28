ade3640512ba35bfdcfb158ab121aebb92262b02
wakamenori <firpmnfiywl@gmail.com>
2022-10-29 03:37:25 +0900

    Fix #7 エピソード詳細ページを作成

--- src/components/navigation/AppBar.tsx
+++ src/components/navigation/AppBar.tsx
@@ -1,21 +1,20 @@
-import {KeyboardEvent, MouseEvent, useState} from 'react'
-import MuiAppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import {KeyboardEvent, MouseEvent, useEffect, useState} from 'react'
+import {AppBar as MuiAppBar, Box, CssBaseline, IconButton, Toolbar} from '@mui/material';
+import {Menu} from '@mui/icons-material'
 import {Logo} from "./Logo";
 import {Drawer} from "./Drawer";
-import CssBaseline from '@mui/material/CssBaseline';
-
+import {EpisodeDetailAppBar} from './EpisodeDetailAppBar'
+import {useRouter} from "next/router";
+import axios, {AxiosResponse} from 'axios'
+import {Summary} from "../../types/episode/summary";
 
 export const AppBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = (event: KeyboardEvent | MouseEvent) => {
     if (
       event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
+      ((event as KeyboardEvent).key === 'Tab' ||
+        (event as KeyboardEvent).key === 'Shift')
     ) {
       return;
     }
@@ -24,11 +23,28 @@ export const AppBar = () => {
   const closeDrawer = () => {
     setIsOpen(false);
   }
+  const router = useRouter();
+  const [episodeTitle, setEpisodeTitle] = useState<string>("");
+  const isEpisodeDetail = router.pathname === '/episode/[episodeId]';
+  useEffect(() => {
+    if (isEpisodeDetail) {
+      const url = `${process.env.NEXT_PUBLIC_API_ROOT}/summary/by_episode/${router.query.episodeId}/`
+      axios.get(url
+      ).then((res: AxiosResponse<Summary>) => {
+        setEpisodeTitle(res.data.title)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }, [router.query, isEpisodeDetail]);
 
   return (
     <Box sx={{display: "flex"}}>
       <CssBaseline/>
-      <MuiAppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+      <MuiAppBar
+        position="fixed"
+        sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}
+      >
         <Toolbar>
           <IconButton
             onClick={toggleDrawer}
@@ -38,13 +54,19 @@ export const AppBar = () => {
             aria-label="open drawer"
             sx={{mr: 2}}
           >
-            <MenuIcon/>
+            <Menu/>
           </IconButton>
           <Logo/>
           {/*<Search/>*/}
         </Toolbar>
+        {isEpisodeDetail && <EpisodeDetailAppBar title={episodeTitle}/>}
       </MuiAppBar>
-      <Drawer open={isOpen} toggleDrawer={toggleDrawer} closeHandler={closeDrawer}/>
+      <Drawer
+        open={isOpen}
+        toggleDrawer={toggleDrawer}
+        closeHandler={closeDrawer}
+        appBarRowNumber={isEpisodeDetail ? 2 : 1}
+      />
     </Box>
   );
 }
