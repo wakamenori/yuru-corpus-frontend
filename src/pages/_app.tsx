57bf2b49f416d4e108ebdf7bca7f7486b71e69b4
wakamenori <firpmnfiywl@gmail.com>
2022-12-07 06:29:36 +0900

    #33 add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 
+import { Analytics } from '@vercel/analytics/react';
 import { AppBar } from '../components/navigation/AppBar'
 import { NotificationContextProvider } from '../context/notification'
 import '../styles/globals.css'
@@ -7,6 +8,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NotificationContextProvider>
+      <Analytics/>
       <AppBar />
       <Component {...pageProps} />
     </NotificationContextProvider>
