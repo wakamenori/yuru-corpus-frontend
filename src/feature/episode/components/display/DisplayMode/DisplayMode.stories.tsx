8243ca7ffb3159fd940ead16fc118f284e9dd56c
sumsat <sums4t@gmail.com>
2022-12-14 17:19:56 +0900

    Add Hidden into DisplayMode stories

--- src/feature/episode/components/display/DisplayMode/DisplayMode.stories.tsx
+++ src/feature/episode/components/display/DisplayMode/DisplayMode.stories.tsx
@@ -9,8 +9,8 @@ export default {
 
 const Template: Story<Props> = (args) => <DisplayMode {...args} />
 
-export const Example = Template.bind({})
-Example.args = {
+export const Visible = Template.bind({})
+Visible.args = {
   speakersInfo: {
     aaa: {
       backgroundColor: 'black',
@@ -32,4 +32,29 @@ Example.args = {
     },
   ],
   display: true,
+}
+
+export const Hidden = Template.bind({})
+Hidden.args = {
+  speakersInfo: {
+    aaa: {
+      backgroundColor: 'black',
+      color: 'white',
+      count: 10,
+      percentage: 90,
+    },
+  },
+  morphemesBySpeaker: [
+    {
+      speaker: 'aaa',
+      morphemes: [
+        {
+          timestamp: '00:00:01',
+          speaker: 'aaa',
+          token: 'aaa',
+        },
+      ],
+    },
+  ],
+  display: false,
 }
