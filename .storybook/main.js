73cfb851b71fdae054515df5afe56d64a66418b5
wakamenori <firpmnfiywl@gmail.com>
2022-12-28 16:19:42 +0900

    npm run storybook できないエラーを解決 & npm audit fix [参考](Error: Cannot find module 'webpack/lib/util/makeSerializable.js')

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  typescript: { reactDocgen: false },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-next-router',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-next-router",
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
 }
