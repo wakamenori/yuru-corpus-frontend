51a9ab8b9e9e268e21c71b51ba9f13261417c827
wakamenori <firpmnfiywl@gmail.com>
2023-01-03 22:36:11 +0900

    Fix #46

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,82 +1,32 @@
 import { Box, CircularProgress } from '@mui/material'
-import axios from 'axios'
-import { GetStaticPropsResult, NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { ChangeEvent, useContext, useEffect, useState } from 'react'
+import { NextPage } from 'next'
+import { ChangeEvent, useContext,  useState } from 'react'
 import { animateScroll as scroll } from 'react-scroll'
 
-import { NotificationContext } from '../../context/notification'
 import { CardList } from '../../feature/episode/components/list/CardList'
 import { Pagination } from '../../feature/episode/components/list/Pagination'
-import { Summary } from '../../types/episode/summary'
+import { SearchContext } from '../../feature/search/context/search'
 
-type Props = {}
 
-type SearchResult = { episodeId: number }
-
-type SearchResponse = {
-  result: SearchResult[]
-}
-const searchApi = async (searchString: string) => {
-  const url = `${process.env.NEXT_PUBLIC_API_ROOT}/search/?string=${searchString}`
-  const response = await axios.get<SearchResponse>(url)
-  return response.data.result
-}
-const summaryApi = async (episodeId: number) => {
-  const url = `${process.env.NEXT_PUBLIC_API_ROOT}/summary/by_episode/${episodeId}/`
-  const response = await axios.get<Summary>(url)
-  return response.data
-}
-
-const Search: NextPage<Props> = ({}) => {
-  const router = useRouter()
-  const [isError, setIsError] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [summary, setSummary] = useState<Summary[]>([])
-  const { notify } = useContext(NotificationContext)
-  useEffect(() => {
-    setSummary([])
-    if (typeof router.query?.string === 'string') {
-      setLoading(true)
-      searchApi(router.query.string)
-        .then((data) => {
-          setIsError(false)
-          setLoading(false)
-          data.map((episode) => {
-            summaryApi(episode.episodeId)
-              .then((data) => {
-                setSummary((prev) => [...prev, data])
-              })
-              .catch((e) => console.log(e))
-          })
-        })
-        .catch((e) => {
-          setIsError(true)
-          setLoading(false)
-          notify('エラーが発生しました もう一度検索してください', 'error')
-          console.log(e)
-        })
-    }
-  }, [router.query, notify])
-
+const Search: NextPage = () => {
+  const { isLoading, searchResult } = useContext(SearchContext)
+  const summary = searchResult
   const [page, setPage] = useState(1)
   const cardPerPage = 10
   const filteredSummary = summary.filter((episode) => {
     return summary.filter((e) => e.id === episode.id).length === 1
   })
-  console.log({ filteredSummary })
   const summarySlice = filteredSummary.slice((page - 1) * cardPerPage, page * cardPerPage)
-  console.log({ summarySlice })
   const totalPages = Math.ceil(filteredSummary.length / cardPerPage)
 
-  const handleChange = (event: ChangeEvent<unknown>, value: number) => {
+  const handleChange = (_: ChangeEvent<unknown>, value: number) => {
     setPage(value)
     scroll.scrollToTop({ duration: 200 })
   }
   return (
     <>
       <Box marginTop={{ xs: 9, sm: 11 }} marginLeft={3}>
-        {loading ? <CircularProgress /> : <p>検索結果:{filteredSummary.length}件</p>}
+        {isLoading && summary.length == 0 ? <CircularProgress /> : <p>検索結果:{filteredSummary.length}件</p>}
       </Box>
       <Box marginBottom={{ xs: 9, sm: 9 }}>
         <CardList summary={summarySlice} />
