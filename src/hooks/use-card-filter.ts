947b1f54f4cbc8e145512164008084a8eed15db2
wakamenori <firpmnfiywl@gmail.com>
2023-01-04 13:55:09 +0900

    #81 フィルタリング条件をクエリに追加

--- src/hooks/use-card-filter.ts
+++ src/hooks/use-card-filter.ts
@@ -1,3 +1,5 @@
+import type { NextRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
 import { Summary } from '../types/episode/summary'
@@ -25,60 +27,104 @@ export type FilterConf = {
   toggleOthers: () => void
 }
 
+const switchFilter = (
+  setFunc: React.Dispatch<React.SetStateAction<boolean>>,
+  newQuery: { [key: string]: boolean },
+  router: NextRouter,
+) => {
+  setFunc((prev) => !prev)
+  router.push({
+    pathname: router.pathname,
+    query: { ...router.query, ...newQuery },
+  })
+}
+
+const getFilter = (router: NextRouter, key: string): boolean => {
+  const query = router.query[key]
+  let filter = false
+  if (typeof query === 'string') {
+    filter = query === 'true'
+  }
+  return filter
+}
+
 export const useCardFilter = (
   summaries: Summary[],
 ): { filterConf: FilterConf; filteredSummaries: Summary[] } => {
+  const router = useRouter()
   const [showGengo, setShowGengo] = useState(true)
-  const toggleGengo = () => {
-    console.log('toggleGengo')
-    setShowGengo((prev) => !prev)
-  }
+  const toggleGengo = () => switchFilter(setShowGengo, { gengo: !showGengo }, router)
   const [showCom, setShowCom] = useState(true)
-  const toggleCom = () => setShowCom((prev) => !prev)
+  const toggleCom = () => switchFilter(setShowCom, { showCom: !showCom }, router)
   const [showShodo, setShowShodo] = useState(true)
-  const toggleShodo = () => setShowShodo((prev) => !prev)
+  const toggleShodo = () => switchFilter(setShowShodo, { showShodo: !showShodo }, router)
   const [showTenmon, setShowTenmon] = useState(true)
-  const toggleTenmon = () => setShowTenmon((prev) => !prev)
+  const toggleTenmon = () => switchFilter(setShowTenmon, { showTenmon: !showTenmon }, router)
   const [showSeitai, setShowSeitai] = useState(true)
-  const toggleSeitai = () => setShowSeitai((prev) => !prev)
+  const toggleSeitai = () => switchFilter(setShowSeitai, { showSeitai: !showSeitai }, router)
   const [showTetsugaku, setShowTetsugaku] = useState(true)
-  const toggleTetsugaku = () => setShowTetsugaku((prev) => !prev)
+  const toggleTetsugaku = () =>
+    switchFilter(setShowTetsugaku, { showTetsugaku: !showTetsugaku }, router)
   const [showOngaku, setShowOngaku] = useState(true)
-  const toggleOngaku = () => setShowOngaku((prev) => !prev)
+  const toggleOngaku = () => switchFilter(setShowOngaku, { showOngaku: !showOngaku }, router)
   const [showMinzoku, setShowMinzoku] = useState(true)
-  const toggleMinzoku = () => setShowMinzoku((prev) => !prev)
+  const toggleMinzoku = () => switchFilter(setShowMinzoku, { showMinzoku: !showMinzoku }, router)
   const [showGakuto, setShowGakuto] = useState(true)
-  const toggleGakuto = () => setShowGakuto((prev) => !prev)
+  const toggleGakuto = () => switchFilter(setShowGakuto, { showGakuto: !showGakuto }, router)
   const [showOthers, setShowOthers] = useState(true)
-  const toggleOthers = () => setShowOthers((prev) => !prev)
+  const toggleOthers = () => switchFilter(setShowOthers, { showOthers: !showOthers }, router)
 
   const [filteredSummaries, setFilteredSummaries] = useState(summaries)
+  useEffect(() => {
+    if (router.isReady) {
+      setShowGengo(getFilter(router, 'showGengo'))
+      setShowCom(getFilter(router, 'showCom'))
+      setShowShodo(getFilter(router, 'showShodo'))
+      setShowTenmon(getFilter(router, 'showTenmon'))
+      setShowSeitai(getFilter(router, 'showSeitai'))
+      setShowTetsugaku(getFilter(router, 'showTetsugaku'))
+      setShowOngaku(getFilter(router, 'showOngaku'))
+      setShowMinzoku(getFilter(router, 'showMinzoku'))
+      setShowGakuto(getFilter(router, 'showGakuto'))
+      setShowOthers(getFilter(router, 'showOthers'))
+    }
+  }, [router.isReady, router.query])
 
   useEffect(() => {
     setFilteredSummaries(
       summaries.filter((summary) => {
         if (!showGengo && summary.channel === 'ゆる言語学ラジオ') {
           return false
-        }else if(!showCom && summary.channel === 'ゆるコンピュータ科学ラジオ'){
+        } else if (!showCom && summary.channel === 'ゆるコンピュータ科学ラジオ') {
           return false
-        }else if(!showShodo && summary.channel === 'ゆる書道学ラジオ'){
+        } else if (!showShodo && summary.channel === 'ゆる書道学ラジオ') {
           return false
-        }else if(!showTenmon && summary.channel === 'ゆる天文学ラジオ'){
+        } else if (!showTenmon && summary.channel === 'ゆる天文学ラジオ') {
           return false
-        }else if(!showSeitai && summary.channel === 'ゆる生態学ラジオ'){
+        } else if (!showSeitai && summary.channel === 'ゆる生態学ラジオ') {
           return false
-        }else if(!showTetsugaku && summary.channel === 'ゆる哲学ラジオ'){
+        } else if (!showTetsugaku && summary.channel === 'ゆる哲学ラジオ') {
           return false
-        }else if(!showOngaku && summary.channel === 'ゆる音楽学ラジオ'){
+        } else if (!showOngaku && summary.channel === 'ゆる音楽学ラジオ') {
           return false
-        }else if(!showMinzoku && summary.channel === 'ゆる民俗学ラジオ'){
+        } else if (!showMinzoku && summary.channel === 'ゆる民俗学ラジオ') {
           return false
         } else {
           return true
         }
       }),
     )
-  }, [summaries, showGengo, showCom, showShodo, showTenmon, showSeitai, showTetsugaku, showOngaku, showMinzoku])
+  }, [
+    summaries,
+    showGengo,
+    showCom,
+    showShodo,
+    showTenmon,
+    showSeitai,
+    showTetsugaku,
+    showOngaku,
+    showMinzoku,
+  ])
 
   return {
     filterConf: {
@@ -92,7 +138,7 @@ export const useCardFilter = (
       toggleTenmon,
       showSeitai,
       toggleSeitai,
-      showTetsugaku ,
+      showTetsugaku,
       toggleTetsugaku,
       showOngaku,
       toggleOngaku,
