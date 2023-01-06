d463bc342571b29da610df57a1b004b68bf559aa
wakamenori <firpmnfiywl@gmail.com>
2023-01-06 18:07:05 +0900

    #108 カードの高さを左右で統一し4個以上の検索結果はボタンでトグル

--- src/components/episode/list/CardList/CardList.stories.tsx
+++ src/components/episode/list/CardList/CardList.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, Story } from '@storybook/react'
 import { CardList, Props } from './CardList'
 
 export default {
-  title: 'components/CardList',
+  title: 'components/card/CardList',
   component: CardList,
 } as Meta<Props>
 
@@ -97,3 +97,124 @@ export const Example = Template.bind({})
 Example.args = {
   summaries: summaries,
 }
+
+
+
+const searchResults: Props['searchResults'] = [
+  {
+      "episodeId": 64,
+      "utterances": [
+        {
+          "speaker": null,
+          "timestamp": "00:07:22",
+          "token": "コンピュータって"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:25",
+          "token": "コンピュータによって魔法がっていう"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:52",
+          "token": "明日から飲み会とかでコンピュータです"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:07:28",
+          "token": "コンピュータという仕事の人いたらしいです"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:08:40",
+          "token": "あんまりコンピュータって言わないんですよね"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:10:03",
+          "token": "コンピュータやってることって"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:07:54",
+          "token": "コンピュータと電卓は違うよ"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:07:10",
+          "token": "コンピュータってどういう5元"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:10:18",
+          "token": "コンピュータは"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:32",
+          "token": "今日皆さんにはコンピュータは"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:21",
+          "token": "ただ落合さんはコンピュータによって"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:47",
+          "token": "コンピュータだと思うんですよ"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:49",
+          "token": "まあいいよコンピュータで俺"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:07:01",
+          "token": "今日からコンピュータ名乗れますね"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:09:07",
+          "token": "僕みたいな僕でさらにコンピュータの"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:10:53",
+          "token": "コンピュータのやってることって"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:07:43",
+          "token": "なんでコンピュータって計算する人なんですよ"
+        }
+      ]
+    },
+    {
+      "episodeId": 65,
+      "utterances": [
+        {
+          "speaker": null,
+          "timestamp": "00:19:55",
+          "token": "01コンピュータの世界で"
+        },
+        {
+          "speaker": null,
+          "timestamp": "00:01:42",
+          "token": "ずっとコンピュータ言語なんですかね"
+        },
+       {
+          "speaker": null,
+          "timestamp": "00:38:08",
+          "token": "という感じで本日は前回に引き続いて 夜コンピュータ科学ラジオ"
+        }
+      ]
+    },
+]
+export const SearchExample = Template.bind({})
+Example.args = {
+  summaries: summaries,
+  searchResults: searchResults
+}
+
