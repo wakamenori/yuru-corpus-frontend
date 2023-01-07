0171505b24bbc48ff0e7d0417a1c48e0d1989ec9
wakamenori <firpmnfiywl@gmail.com>
2023-01-07 15:40:37 +0900

    #124 add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,21 @@
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 
-// import { GA } from '../components/GA'
+import { GA } from '../components/GA'
 import { AppBar } from '../components/navigation/AppBar'
 import { HistoryContextProvider } from '../context/history'
 import { NotificationContextProvider } from '../context/notification'
 import { SearchContextProvider } from '../feature/search/context/search'
-// import { usePageView } from '../hooks/use-page-view'
+import { usePageView } from '../hooks/use-page-view'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // usePageView()
+  usePageView()
   return (
     <HistoryContextProvider>
       <NotificationContextProvider>
         <SearchContextProvider>
-          {/* <GA/> */}
+          <GA/>
           <Analytics />
           <AppBar />
           <Component {...pageProps} />
